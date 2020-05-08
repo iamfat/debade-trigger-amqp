d4b00de05e87bd3ce5728b11f73cbbd51aa7ff38
Jia Huang <iamfat@gmail.com>
2020-05-08 12:28:42 +0800

    use nanoid to specify queue name

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const amqp = require("amqplib");
 const config_1 = require("./lib/config");
 const callback_1 = require("./lib/callback");
+const non_secure_1 = require("nanoid/non-secure");
 console.info('>> DeBaDe AMQP Trigger <<');
 async function run() {
     console.info(`connecting to ${config_1.default.server}...`);
@@ -21,25 +22,33 @@ async function run() {
             type = type || 'fanout';
             routing_keys = routing_keys || [''];
             const channel = await conn.createChannel();
-            await channel.assertExchange(exchange, type, {
-                durable: false,
-                autoDelete: true,
-            });
-            const { queue } = await channel.assertQueue('', {
-                exclusive: false,
-                autoDelete: true,
-            });
-            await Promise.all(routing_keys.map((routing_key) => channel.bindQueue(queue, exchange, routing_key)));
-            await channel.prefetch(1);
-            await channel.consume(queue, (message) => {
-                const { fields, content } = message;
-                const tag = `${fields.exchange}/${fields.routingKey}/${fields.deliveryTag}`;
-                const data = content.toString('utf-8');
-                console.debug(`<< message from ${tag} len=${data.length}`);
-                Promise.all(callbacks.map((options) => callback_1.default(data, options))).finally(() => {
-                    channel.ack(message);
+            const queue = non_secure_1.nanoid();
+            try {
+                await Promise.all([
+                    channel.prefetch(1),
+                    channel.assertExchange(exchange, type, {
+                        durable: false,
+                        autoDelete: true,
+                    }),
+                    channel.assertQueue(queue, {
+                        exclusive: false,
+                        autoDelete: true,
+                    }),
+                ]);
+                await Promise.all(routing_keys.map((routing_key) => channel.bindQueue(queue, exchange, routing_key)));
+                await channel.consume(queue, (message) => {
+                    const { fields, content } = message;
+                    const tag = `${fields.exchange}/${fields.routingKey}/${fields.deliveryTag}`;
+                    const data = content.toString('utf-8');
+                    console.debug(`<< message from ${tag} len=${data.length}`);
+                    Promise.all(callbacks.map((options) => callback_1.default(data, options))).finally(() => {
+                        channel.ack(message);
+                    });
                 });
-            });
+            }
+            catch (e) {
+                console.error((e === null || e === void 0 ? void 0 : e.message) || String(e));
+            }
         });
     }
     catch (e) {
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as amqp from 'amqplib';
 import config from './lib/config';
 import callback from './lib/callback';
+import { nanoid } from 'nanoid/non-secure';
 
 console.info('>> DeBaDe AMQP Trigger <<');
 
@@ -25,29 +26,37 @@ async function run() {
       routing_keys = routing_keys || [''];
 
       const channel = await conn.createChannel();
-      await channel.assertExchange(exchange, type, {
-        durable: false,
-        autoDelete: true,
-      });
+      const queue = nanoid();
 
-      const { queue } = await channel.assertQueue('', {
-        exclusive: false,
-        autoDelete: true,
-      });
+      try {
+        await Promise.all([
+          channel.prefetch(1),
+          channel.assertExchange(exchange, type, {
+            durable: false,
+            autoDelete: true,
+          }),
+          channel.assertQueue(queue, {
+            exclusive: false,
+            autoDelete: true,
+          }),
+        ]);
 
-      await Promise.all(routing_keys.map((routing_key) => channel.bindQueue(queue, exchange, routing_key)));
-      await channel.prefetch(1);
+        await Promise.all(routing_keys.map((routing_key) => channel.bindQueue(queue, exchange, routing_key)));
 
-      await channel.consume(queue, (message) => {
-        const { fields, content } = message;
-        const tag = `${fields.exchange}/${fields.routingKey}/${fields.deliveryTag}`;
-        const data = content.toString('utf-8');
+        await channel.consume(queue, (message) => {
+          const { fields, content } = message;
+          const tag = `${fields.exchange}/${fields.routingKey}/${fields.deliveryTag}`;
+          const data = content.toString('utf-8');
 
-        console.debug(`<< message from ${tag} len=${data.length}`);
-        Promise.all(callbacks.map((options) => callback(data, options))).finally(() => {
-          channel.ack(message);
+          console.debug(`<< message from ${tag} len=${data.length}`);
+          Promise.all(callbacks.map((options) => callback(data, options))).finally(() => {
+            channel.ack(message);
+          });
         });
-      });
+      } catch (e) {
+        // DO NOTHING
+        console.error(e?.message || String(e));
+      }
     });
   } catch (e) {
     console.error('error: ', e?.message || String(e));
